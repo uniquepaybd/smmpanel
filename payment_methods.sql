10abf056dca435bdea7d040d6f1e31460e1df053
uniquepaybd <145537761+uniquepaybd@users.noreply.github.com>
2023-09-20 00:11:11 -0700

    Create payment_methods.sql

---
+++ payment_methods.sql
@@ -0,0 +1,2 @@
+INSERT INTO `payment_methods` (`id`, `method_name`, `method_get`, `method_min`, `method_max`, `method_type`, `method_extras`, `method_line`) VALUES
+(44, 'uniquepaybd', 'uniquepaybd', 1, 10000, '2', '{\"method_type\":\"1\",\"name\":\"uniquepaybd\",\"min\":\"1\",\"max\":\"10000\",\"api_key\":\"12345\",\"secret_key\":\"12345\",\"host_name\":\"exemple.com\",\"currency_rate\":\"85\"}', 1);
